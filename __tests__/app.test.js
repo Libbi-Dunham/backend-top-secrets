5ac17f2659cd76eee82f7a8fc056eddf4fe40716
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-03-29 14:53:32 -0700

    wrote out test, controll,model for sign in(post)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('backend-top-secrets routes', () => {
   beforeEach(() => {
@@ -20,6 +21,22 @@ describe('backend-top-secrets routes', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       email: 'miklo@test.com',
+    });
+
+    it('signs in an existing user', async () => {
+      const user = await UserService.create({
+        email: 'miklo@test.com',
+        password: 'ilovetreats',
+      });
+
+      const res = await request(app)
+        .post('/api/v1/user/session')
+        .send({ email: 'miklo@test.com', password: 'ilovetreats' });
+
+      expect(res.body).toEqual({
+        message: 'successfully signed in',
+        user,
+      });
     });
   });
 });
